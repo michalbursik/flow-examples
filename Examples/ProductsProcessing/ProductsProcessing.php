334d7cafc3ec10dc01e3a12311a9dbaeb4bbc3c2
Michal Bursík <michalbursik95@gmail.com>
2024-09-12 12:53:14 +0200

    E-HEAD: Example: ProductsProcessing

--- Examples/ProductsProcessing/ProductsProcessing.php
+++ Examples/ProductsProcessing/ProductsProcessing.php
@@ -6,11 +6,14 @@ use Flow\ETL\DataFrame;
 use Flow\ETL\Filesystem\SaveMode;
 use Flow\ETL\Join\Expression;
 
+use Flow\ETL\Row;
+
 use function Flow\ETL\Adapter\CSV\to_csv;
 use function Flow\ETL\Adapter\XML\from_xml;
 use function Flow\ETL\DSL\data_frame;
 use function Flow\ETL\DSL\equal;
 use function Flow\ETL\DSL\from_array;
+use function Flow\ETL\DSL\int_entry;
 use function Flow\ETL\DSL\lit;
 use function Flow\ETL\DSL\ref;
 use function Flow\ETL\DSL\when;
@@ -20,15 +23,15 @@ class ProductsProcessing
 
     // Source XML file & its configuration
     const string XML_NODE_PATH = 'products/product';
-    const string SOURCE_XML_FILEPATH_FROM_ROOT = 'Examples/ProductsProcessing/data/products_50/new_products.xml';
+    const string SOURCE_XML_FILEPATH_FROM_ROOT = 'Examples/ProductsProcessing/data/products_complex/new_products.xml';
 
     // Products inserted after source XML file is processed (DB creates IDs), used for joining with product variants
-    const string EXISTING_PRODUCTS_AFTER_INSERT_RELATIVE_FILEPATH = 'data/products_50/existing_products_after_insert.php';
+    const string EXISTING_PRODUCTS_AFTER_INSERT_RELATIVE_FILEPATH = 'data/products_complex/existing_products_after_insert.php';
 
     // Database mocks: existing products and product variants (e.g. from previous imports)
     // So far no need to use - its here for better imagination of how it will work
-    const string EXISTING_PRODUCTS_RELATIVE_FILEPATH = 'data/products_50/existing_products.php';
-    const string EXISTING_PRODUCT_VARIANTS_RELATIVE_FILEPATH = 'data/products_50/existing_product_variants.php';
+    const string EXISTING_PRODUCTS_RELATIVE_FILEPATH = 'data/products_complex/existing_products.php';
+    const string EXISTING_PRODUCT_VARIANTS_RELATIVE_FILEPATH = 'data/products_complex/existing_product_variants.php';
 
     public function run()
     {
@@ -88,12 +91,6 @@ class ProductsProcessing
             // Grouping key
             'GROUPING_KEY' => ref('node')->xpath('parent_product_id')->domElementValue(),
 
-            'GTIN' => when(
-                ref('node')->xpath('ean')->domElementValue()->equals(lit('NULL')),
-                lit(''),
-                ref('node')->xpath('ean')->domElementValue()
-            ),
-
             // There will be more processing e.g.: (not implemented yet - still discovering how the ETL lib works)
             // - categories: "Man > Shirts > Polo" -> DB structure:
             //      Man category -> Shirts category (Man category id) -> Polo category (Shirts category id)"
@@ -144,8 +141,8 @@ class ProductsProcessing
         $newProductVariantsDF->withEntries([
             'EXTERNAL_ID' => ref('node')->xpath('merchant_product_id')->domElementValue(),
             'GROUP_ID' => ref('node')->xpath('parent_product_id')->domElementValue(),
-            'GTIN' => ref('node')->xpath('ean')->domElementValue(),
-            'MPN' => ref('node')->xpath('model_number')->domElementValue(),
+            'GTIN' => ref('node')->xpath('product_GTIN')->domElementValue(),
+            'MPN' => ref('node')->xpath('mpn')->domElementValue(),
             'NAME' => ref('node')->xpath('product_name')->domElementValue(),
             'GENDER' => ref('node')->xpath('suitable_for')->domElementValue(),
             'IMAGE_URL' => ref('node')->xpath('merchant_image_url')->domElementValue(),
@@ -181,7 +178,7 @@ class ProductsProcessing
         // There should always be some existing product variant as products are created from product_variants
         $existingProductsDF = data_frame()->read(from_array($existingProductsAfterInsert));
         $newProductVariantsDF->join($existingProductsDF, Expression::on(equal('GROUPING_KEY', 'GROUPING_KEY')));
-        $newProductVariantsDF->rename('joined_ID', 'ID');
+        $newProductVariantsDF->rename('joined_ID', 'PRODUCT_ID');
         // Need to drop since joining columns needs to be unique.
         $newProductVariantsDF->drop('joined_GROUPING_KEY');
 
@@ -190,7 +187,7 @@ class ProductsProcessing
         $columnsToDrop = [];
         if ($existingProductVariants) {
             $existingProductVariantsDF = data_frame()->read(from_array($existingProductVariants));
-            $newProductVariantsDF->join($existingProductVariantsDF, Expression::on(equal('GROUPING_KEY', 'GROUPING_KEY')));
+            $newProductVariantsDF->join($existingProductVariantsDF, Expression::on(equal('EXTERNAL_ID', 'EXTERNAL_ID')));
 
             // Add the ID column based on the joined values -> ensures the product variants are updated instead of insert
             $newProductVariantsDF->withEntry('ID',
@@ -201,7 +198,7 @@ class ProductsProcessing
                 )
             );
 
-            $columnsToDrop = ['joined_ID', 'joined_GROUPING_KEY'];
+            $columnsToDrop = ['joined_ID', 'joined_EXTERNAL_ID'];
         }
 
         $newProductVariantsDF->drop('node', 'IN_STOCK', ...$columnsToDrop);
--- Examples/ProductsProcessing/data/products_50/existing_products.php
+++ Examples/ProductsProcessing/data/products_50/existing_products.php
@@ -1,7 +1,21 @@
 <?php
 
-// Could simulate existing products from previous import
-// - test that existing product is overwritten by new values (but keeps the database ID)
 return [
-
+    [
+        'ID' => 2,
+        'EXTERNAL_ID' => '136-10-02010.4',
+        'GROUP_ID' => 1035261,
+        'NAME' => 'Freizeit Schnürer weiss Plateau Sneaker 41',
+        'IMAGE_URL' => 'https://www.schuhe24.de/media/image/c2/d3/d5/361895_2.jpg',
+        'PRICE' => 270.64,
+        'CURRENCY' => 'EUR',
+        'URL' => 'https://www.schuhe24.de/herren/sneaker-schnuerer/freizeit-schnuerer/j.w.anderson-freizeit-schnuerer-weiss-plateau-sneaker-136-10-02010',
+        'BRAND' => 'J.W.Anderson',
+        'DESCRIPTION' => 'Dieser schöne Schuh der Qualitätsmarke J.W.Anderson und der Farbe weiss ist perfekt für jeden Anlass geeignet und in den Größen 38,41 erhältlich. Der Schuh gehört zur Kategorie der Freizeit Schnürer.',
+        'SHOP_ID' => 1,
+        'FEED_ID' => 1,
+        'UPDATED' => '2024-09-11 10:11:21',
+        'GROUPING_KEY' => 1035261,
+        'GTIN' => null,
+    ]
 ];
